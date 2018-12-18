9ebb5222976e11aff72fd4f3f26f916b3ec261c1
muhwezi deo <aggrey256@gmail.com>
2018-12-18 19:36:34 +0300

    state

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -3,13 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 class App extends Component {
+  // states are on only avialable in class components not function components
+  state={
+    persons:[
+      {name:"dee", age:"24"},
+      {name:"deo", age:"27"},
+      {name:"aggrey", age:"34"},
+
+    ]
+  
+  }
   render() {
     return (
     <div className="App">
       <h1>Hello</h1>
-      <Person name="deo" age="25"/>
-      <Person name="dee" age="28"/>
-      <Person name="aggrey" age="32">My hobbies: racing</Person>
+      <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
     );
     // return React.createElement('div',{className:'App'},React.createElement('h1',null,'updated'))
