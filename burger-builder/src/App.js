dbd740084265bd36fb2cb1f5018f18a40b43f0e8
muhwezi deo <aggrey256@gmail.com>
2018-12-19 07:41:50 +0300

    calling methods from other components

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
       <h1>Hello</h1>
       <button onClick={this.switchNameHandler}>Click Me</button>
       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-      <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+      <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler}/>
       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
     </div>
     );
--- burger-builder/src/Person/Person.jsx
+++ burger-builder/src/Person/Person.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 const person =(props) =>{
  return(
      <div>
-     <p>Im a {props.name} and am {Math.floor(Math.random()*props.age)}</p>
+     <p onClick={props.click}>Im a {props.name} and am {Math.floor(Math.random()*props.age)}</p>
      <p>{props.children}</p>
      </div>
  )
