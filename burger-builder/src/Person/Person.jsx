82f20501be2cb41cb841aa1ca7f3efa320f2d167
muhwezi deo <aggrey256@gmail.com>
2018-12-19 09:07:55 +0300

    add stylesheet

--- burger-builder/src/Person/Person.jsx
+++ burger-builder/src/Person/Person.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Person.css';
 const person =(props) =>{
  return(
-     <div>
+     <div className='Person'>
      <p onClick={props.click}>Im a {props.name} and am {Math.floor(Math.random()*props.age)}</p>
      <p>{props.children}</p>
      <input type="text" value={props.name} onChange={props.changed}  />
