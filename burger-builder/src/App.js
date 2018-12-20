67411d07959cc43dea6550c0fb22cb6863474f94
muhwezi deo <aggrey256@gmail.com>
2018-12-20 08:09:59 +0300

    use dynamic array

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -56,9 +56,15 @@ class App extends Component {
     if(this.state.showPersons){
       persons=(
         <div>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          {this.state.persons.map(
+            person=>{
+              return <Person name={person.name} age={person.age}/>
+            }
+          )};
+
+        {/* <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switchNameHandler.bind(this,'deo the bind way')}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.changeNameHandler} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} changed={this.changeNameHandler} /> */}
         </div>
       )
     }
