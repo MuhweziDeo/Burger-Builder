c69fb4a4f9778492c87336399753b0d659e4da86
muhwezi deo <aggrey256@gmail.com>
2018-12-21 07:38:46 +0300

    restructure project folder

--- burger-builder/src/containers/App.js
+++ burger-builder/src/containers/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+ import React, { Component } from 'react';
+
 import './App.css';
-import Person from './Person/Person';
+import Person from  '../components/Persons/Person/Person'
 class App extends Component {
   // states are on only avialable in class components not function components
   state={
