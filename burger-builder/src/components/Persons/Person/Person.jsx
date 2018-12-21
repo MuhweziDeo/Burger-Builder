7a21912db9ad15429d638037795969601b352df6
muhwezi deo <aggrey256@gmail.com>
2018-12-21 12:02:21 +0300

    convert stateless to stateful

--- burger-builder/src/components/Persons/Person/Person.jsx
+++ burger-builder/src/components/Persons/Person/Person.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './Person.css';
-const person =(props) =>{
- return(
-     <div className='Person'>
-     <p onClick={props.click}>Im a {props.name} and am {Math.floor(Math.random()*props.age)}</p>
-     <p>{props.children}</p>
-     <input type="text" value={props.name} onChange={props.changed}  />
-     </div>
- )
+
+class Persons extends Component{
+    render(){
+        return(
+            <div className='Person'>
+            <p onClick={this.props.click}>Im a {this.props.name} and am {Math.floor(Math.random()*this.props.age)}</p>
+             <p>{this.props.children}</p>
+           <input type="text" value={this.props.name} onChange={this.props.changed}  />
+             </div>
+        )
+    }
 }
-export default person;
+// const person =(props) =>{
+//  return(
+//      <div className='Person'>
+//      <p onClick={props.click}>Im a {props.name} and am {Math.floor(Math.random()*props.age)}</p>
+//      <p>{props.children}</p>
+//      <input type="text" value={props.name} onChange={props.changed}  />
+//      </div>
+//  )
+// }
+export default Persons;
 // use function based components
