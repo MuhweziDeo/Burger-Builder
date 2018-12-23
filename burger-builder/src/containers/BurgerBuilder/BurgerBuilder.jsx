9484e5e6d346515aee347eb1258ef25801ad73e8
muhwezideo <aggrey256@gmail.com>
2018-12-23 21:21:43 +0300

    output burger ingredients dynamically

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -13,7 +13,7 @@ class BurgerBuilder extends Component{
     render(){
         return(
            <Aux>
-               <Burger/>
+               <Burger ingredients={this.state.ingredients}/>
                <div>Burger controls</div>
            </Aux>
         )
