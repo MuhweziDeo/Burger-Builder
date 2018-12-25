3e159371912ede3cd494520e12164be0136528e9
muhwezideo <aggrey256@gmail.com>
2018-12-25 20:19:08 +0300

    add side drawer toogle

--- burger-builder/src/components/Layout/Layout.jsx
+++ burger-builder/src/components/Layout/Layout.jsx
@@ -5,15 +5,20 @@ import ToolBar from '../Navigation/ToolBar/ToolBar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 class Layout extends Component{
   state={
-    showSideDrawer:true
+    showSideDrawer:false
   }
   sideDrawerClosedHandler=()=>{
     this.setState({showSideDrawer:false});
   }
+  SideDrawerToggleHandler=()=>{
+    this.setState((prevState)=>{
+    return {showSideDrawer:!prevState.showSideDrawer};
+  });
+  }
   render(){
     return(
       <Aux>
-        <ToolBar/>
+        <ToolBar drawerToggleClicked={this.SideDrawerToggleHandler}/>
         <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler}/>
           <div>Toolbar,sidebar,Backdrop</div>
           <main className={classes.Content}>
--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.jsx
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.jsx
@@ -4,6 +4,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
+
 const sideDrawer=(props)=>{
   let attachedClasses=[classes.SideDrawer,classes.Close]
   if (props.open){
@@ -11,7 +12,6 @@ const sideDrawer=(props)=>{
   }
   console.log(attachedClasses);
   return(
-
     <Aux>
       <Backdrop show={props.open} clicked={props.closed}/>
     <div className={attachedClasses.join(' ')}>
